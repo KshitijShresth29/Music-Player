5ec1aece4314ec92fd8b9386025a57aaf76169de
Kshitij Shresth <145615126+KshitijShresth29@users.noreply.github.com>
2024-01-30 22:28:46 +0000

    Update Playlist.js

--- Playlist.js
+++ Playlist.js
@@ -1,1 +1,30 @@
+const mongoose = require("mongoose");
+const Playlist = new mongoose.Schema({
+    name:{
+        type: String,
+        required: true,
 
+    },
+    thumbnail:{
+        type: String,
+        required: false,
+    },
+
+    owner :{
+        type: mongoose.Types.ObjectId,
+        ref: "user",      
+    },
+    songs:[{
+        type: mongoose.Types.ObjectId,
+        ref:"song",
+        },
+],
+collaborators:[{
+    type: mongoose.Types.ObjectId,
+    ref: "user"
+},],
+
+   
+});
+const PlaylistModel = mongoose.model("Playlist", Playlist);
+module.exports = PlaylistModel;
