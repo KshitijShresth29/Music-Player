cfe602e1446ced9b3f3e8a12765c18cf9babd797
Kshitij Shresth <145615126+KshitijShresth29@users.noreply.github.com>
2024-02-06 17:39:07 +0000

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
-
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/Home/Home";
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Home />} />
+            </Routes>
+        </Router>
+    );
+};
 
-export default App
+export default App;
